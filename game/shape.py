4439716e6b1d635f1821c2602014eed58a12a7e9
Logan Parker <lopar6@gmail.com>
2021-04-23 16:17:31 -0600

    Feature: Added parent class shapes

---
+++ game/shape.py
@@ -0,0 +1,36 @@
+class Shape():
+    
+    facing_direction = "up"
+
+    def __init__(self):
+        self.facing_direction = "up"
+
+    def spin(self):
+        if self.facing_direction == "up":
+            return get_facing_right()
+
+        if self.facing_direction == "right":
+            return get_facing_down()
+
+        if self.facing_direction == "down": 
+            return get_facing_left()
+
+        if self.facing_direction == "left":
+            return get_facing_up()
+
+    def get_facing_up(self):
+        raise NotImplementedError
+
+    def get_facing_down(self):
+        raise NotImplementedError
+
+    def get_facing_left(self):
+        raise NotImplementedError
+
+    def get_facing_right(self):
+        raise NotImplementedError
+
+class Square(Shape):
+    
+   
+        
--- game/shapes.py
+++
@@ -1,3 +0,0 @@
-
-class Shapes():
-    pass
