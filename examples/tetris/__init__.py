621e573b52aa13c7950f3c7adf45a1d06b2f55b9
Nate Brooks <nateonguitar@gmail.com>
2021-04-24 11:23:47 -0600

    set up the ability to choose an example game on main launch

---
+++ examples/tetris/__init__.py
@@ -0,0 +1,3 @@
+from game import Game
+class TetrisGame(Game):
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from game import Game
+from examples.tetris import TetrisGame
 
-game = Game()
-game.start()
+
+class AvailableGame():
+    def __init__(self, label, game_class, description=''):
+        self.label = label
+        self.description = description
+        self.game_class = game_class
+
+
+
+available_games = (
+    AvailableGame(label='Tetris', game_class=TetrisGame, description="It's Tetris baby!"),
+)
+
+for i in range(0, len(available_games)):
+    g = available_games[i]
+    print(f'{i}: {g.label} | {g.description}')
+
+try:
+    choice = input('Choose a game by number: ')
+    choice_int = int(choice)
+    if choice_int < 0:
+        raise Exception('Input not in range')
+    GameChoice = available_games[choice_int].game_class
+    game = GameChoice()
+    game.start()
+except:
+    print('\nbad input\n')
